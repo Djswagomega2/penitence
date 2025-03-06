c4417fee6f53643905d168a40c67167d3464bf6e
Daniel Glenn-Schultz <nycsubwayguy2@gmail.com>
2025-03-06 18:02:18 -0500

    Testing oo not merge

--- Assets/Scripts/States/Attack States/TouchAttackState.cs
+++ Assets/Scripts/States/Attack States/TouchAttackState.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class TouchAttackState : State
 {
-    //TODO: Attack is happening too fast 
     #region General
     [Header("General")]
     [SerializeField] private Rigidbody2D rb;
@@ -34,7 +33,6 @@ public class TouchAttackState : State
 
     void Start()
     {
-
         aiDestinationSetter = enemy.GetComponent<AIDestinationSetter>();
         aiPath = enemy.GetComponent<AIPath>();
         aiLerp = enemy.GetComponent<AILerp>();
@@ -42,7 +40,7 @@ public class TouchAttackState : State
         playerTransform = player.transform;
         ps = player.GetComponent<PlayerScript>();
     }
-public override State RunCurrentState()
+    public override State RunCurrentState()
     {
         aiLerp.speed = touchAttackSpeed;
         if (!hasTouchAttacked)
@@ -51,7 +49,7 @@ public override State RunCurrentState()
             aiPath.enabled = false;
             StartCoroutine(TouchAttack());
         }
-        else 
+        else
         {
             StopCoroutine(TouchAttack());
             aiDestinationSetter.enabled = true;
@@ -61,9 +59,8 @@ public override State RunCurrentState()
             return pursuitState;
         }
 
-        return this; 
+        return this;
     }
-    //Lets first make the enemy's speed zero when we start on the attack as to indicate to player that the enemy is doing the attack. Afterwards, we commit to the attack like usual
 
     private IEnumerator TouchAttack() 
     {
@@ -74,11 +71,10 @@ public override State RunCurrentState()
     }
 
 
-    private void Lunge() 
+    private void Lunge()
     {
         Vector2 directionToPlayer = ((Vector2)ps.droplet.transform.position - rb.position).normalized;
-        rb.velocity = directionToPlayer * aiLerp.speed;
-        Debug.Log("I AM EDGING!");
+        rb.velocity = directionToPlayer * aiLerp.speed; ;
     }
 
 }
