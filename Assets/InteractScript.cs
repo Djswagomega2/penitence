12590e98c536d24044acf47ea8f0d4b6c84cfa66
gamingnam <namhieudang2008@gmail.com>
2025-04-05 16:13:06 -0400

    Added universal interactor, started working on doors and more scalable usage. (remember to delete assignItemToObjectScript)

---
+++ Assets/InteractScript.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+interface IInteractable
+{
+    public void Interact();
+}
+public class InteractScript : MonoBehaviour
+{
+    public GameObject textHolder;
+    public int interactRange;
+    public Collider2D[] interactableList;
+    public int selectedIndex;
+    public LayerMask _layerMask;
+
+    void Start()
+    {
+        selectedIndex = 0;
+        interactRange = 2;
+    }
+    void Update()
+    {
+        interactableList = Physics2D.OverlapCircleAll(transform.position, interactRange, _layerMask);
+        
+        if (interactableList.Length > 0)
+        {
+            textHolder.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Press E to interact " + interactableList[selectedIndex].name;
+            //handles more than 1 interactable
+            if (interactableList.Length > 1)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                {
+                    selectedIndex = Mathf.Clamp(selectedIndex + 1, 0, interactableList.Length-1);
+                }
+                else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                {
+                    selectedIndex = Mathf.Clamp(selectedIndex - 1, 0, interactableList.Length-1);
+                }
+                //add text to display use of scrollwheel to change what u wanna interact with
+            }
+            textHolder.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (interactableList[selectedIndex].gameObject.TryGetComponent(out IInteractable interactGameObject))
+                {
+                    interactGameObject.Interact();
+                    if (selectedIndex != 0) selectedIndex -= 1;
+                }
+            }
+        }
+        else
+        {
+            textHolder.SetActive(false);
+
+        }
+    }
+}
--- Assets/Scripts/Items/ItemClass.cs
+++ Assets/Scripts/Items/ItemClass.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemClass : ScriptableObject
@@ -10,8 +11,10 @@ public class ItemClass : ScriptableObject
     public string description;
     public bool isStackable = true;
     public bool isDroppable;
-    public GameObject itemObject;
     public float equipSpeed;
+
+    private GameObject inventoryManager;
+    private GameObject itemGameObject;
 
     public virtual void Use(PlayerScript caller) // rename method
     {
