9c1f5e239030082ec652ad3a2e6ade21cd80d208
Bryan Trukhin <bryantrukhin77@gmail.com>
2025-05-04 16:14:37 -0400

    Level One is 99% done

--- Assets/LevelOneManager.cs
+++ Assets/LevelOneManager.cs
@@ -10,7 +10,7 @@ public class LevelOneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        generatorAmountTurnedOn = 0;
+
     }
 
     // Update is called once per frame
