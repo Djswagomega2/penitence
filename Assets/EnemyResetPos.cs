db9c599e5999fb9e56e69e752e4883e472101026
Daniel Glenn-Schultz <nycsubwayguy2@gmail.com>
2025-06-07 11:48:20 -0400

    Level 3 for Toto

--- Assets/EnemyResetPos.cs
+++ Assets/EnemyResetPos.cs
@@ -48,26 +48,25 @@ public class EnemyResetPos : MonoBehaviour
 			var aiDestSetter = enemy.GetComponent<AIDestinationSetter>();
 			var aiPath = enemy.GetComponent<AIPath>();
 			StateManager stateManager = enemy.GetComponent<StateManager>();
-<<<<<<< HEAD
+
 			
-=======
+
 			State wanderState = enemy.transform.GetChild(1).transform.GetChild(0).GetComponent<WanderState>();
->>>>>>> origin/SkibidiDanielBranch
+
 
             // Fully disable chasing behavior
             if (fov != null) fov.enabled = false;
 			if (aiDestSetter != null) aiDestSetter.target = null;
 			if (aiPath != null) aiPath.enabled = false;
-<<<<<<< HEAD
+
 			if(stateManager != null) stateManager.currentState = enemy.transform.GetChild(1).transform.GetChild(0).GetComponent<WanderState>();// Set to idle or appropriate state
 			Debug.Log("Disabling AI for enemy: " + stateManager.currentState);
-=======
+
 			if(stateManager != null)
 			{
 				stateManager.SwitchToTheNextState(wanderState); // Switch to Wander state
 				stateManager.enabled = false; // Disable state manager to prevent state changes
 			}
->>>>>>> origin/SkibidiDanielBranch
 
 
             // Reset position manually
